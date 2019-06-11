346a6a421207befce655208d91ac74ee77e3acca
Ravi Kowlessur <ravi.kowlessur@gmail.com>
2019-06-11 11:29:45 +0400

    Test MockMvc

--- src/test/java/ravi/springdemo/recipe/controllers/IndexControllerTest.java
+++ src/test/java/ravi/springdemo/recipe/controllers/IndexControllerTest.java
@@ -4,6 +4,9 @@ import static org.junit.Assert.assertEquals;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 
 import java.util.HashSet;
 import java.util.Set;
@@ -14,6 +17,9 @@ import org.mockito.ArgumentCaptor;
 import org.mockito.ArgumentMatchers;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MockMvcBuilder;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.ui.Model;
 
 import ravi.springdemo.recipe.model.Recipe;
@@ -63,4 +69,12 @@ public class IndexControllerTest {
 		assertEquals(2, arguementCaptor.getValue().size());
 	}
 
+	@Test
+	public void testMockMvc() throws Exception{
+		MockMvc mockMvc = MockMvcBuilders.standaloneSetup(indexController).build();
+		mockMvc.perform(get("/"))
+			   .andExpect(status().isOk())
+			   .andExpect(view().name("index"));
+	}
+	
 }
